a1643dcaf4c278c1f350d92356f3605176424419
broux <bapt.roux@gmail.com>
2018-08-03 00:24:34 +0200

    Correct path in init script

--- cnfLoad.sh
+++ cnfLoad.sh
@@ -5,5 +5,5 @@ SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 # Export awesome folder as simlink
 ################################################################################
 ### Awesome config
-awesome_trgt_path=~/.config/nvim/init.vim
+awesome_trgt_path=~/.config/awesome
 ln -i -s $SCRIPTPATH $awesome_trgt_path
